5d5dd76f2d78a003195f3e9551f18a8c762413ef
Mate Soos <soos.mate@gmail.com>
2025-03-29 11:11:02 +0100

    Update emscripten

--- build_emscripten.sh
+++ build_emscripten.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-CXXFLAGS=-fPIC ./configure
+export CXX=/usr/lib/emscripten/em++
+export CC=/usr/lib/emscripten/emcc
+export AR=/usr/lib/emscripten/emar
+export CXXFLAGS=-fPIC
+./configure
 make -f makefile.emscripten
