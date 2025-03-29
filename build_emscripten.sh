ba41af09bd9a30363d13750f002e9ae4aeb04ccc
Mate Soos <soos.mate@gmail.com>
2025-03-29 12:09:16 +0100

    Update emscripten

--- build_emscripten.sh
+++ build_emscripten.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-export CXX=/usr/lib/emscripten/em++
-export CC=/usr/lib/emscripten/emcc
-export AR=/usr/lib/emscripten/emar
-export CXXFLAGS=-fPIC
-./configure
-make -f makefile.emscripten
+rm -f libcadi*
+emmake make -f makefile.emscripten
+cp libcadi* $EMINSTALL/lib/
