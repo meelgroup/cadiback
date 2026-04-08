229e402b0e8b29ee85835fd676ce4f26e6f5d76d
Mate Soos <soos.mate@gmail.com>
2026-04-08 20:13:10 +0200

    Fix build system

--- build_release.sh
+++ build_release.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -e
+SAT_DIR=$(cd .. && pwd)
 cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF \
-  -Dcadical_DIR=../cadical/build \
+  -Dcadical_DIR=${SAT_DIR}/cadical/build \
   -DCMAKE_INSTALL_PREFIX="$(pwd)/build"
 cmake --build build -j$(nproc)
 cmake --install build
--- build_static_release.sh
+++ build_static_release.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -e
 rm -rf build
+SAT_DIR=$(cd .. && pwd)
 cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF \
-  -Dcadical_DIR=../cadical/build \
+  -Dcadical_DIR=${SAT_DIR}/cadical/build \
   -DCMAKE_INSTALL_PREFIX="$(pwd)/build"
 cmake --build build -j$(nproc)
 cmake --install build
