7fe1f8cb553c421114b1bd53a643dcaa7b761bdc
Mate Soos <soos.mate@gmail.com>
2026-05-24 10:30:21 +0200

    Add notest

---
+++ build_notest.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+rm -rf .cmake
+rm -rf lib*
+rm -rf Test*
+rm -rf tests*
+rm -rf include
+rm -rf tests
+rm -rf approxmc*
+rm -rf apx-src
+rm -rf CM*
+rm -rf cmake*
+rm -rf deps
+rm -rf _deps
+SAT_DIR=$(cd ../.. && pwd)
+cmake -S .. -B . -DCMAKE_BUILD_TYPE=RelWithDebInfo -DENABLE_TESTING=OFF \
+  -Dcadical_DIR="${SAT_DIR}/cadical/build" \
+  -DCMAKE_INSTALL_PREFIX="$(pwd)"
+cmake --build . -j$(nproc)
+cmake --install .
